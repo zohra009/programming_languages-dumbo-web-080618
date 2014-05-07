6f7b0f26ee497781e9e650f84565823f0a245d4e
Blake Johnson <blake41@gmail.com>
2014-05-07 09:43:27 -0400

    solution

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-
+  new_languages = {}
+  languages.each do |type, hash|
+    hash.each do |name, attributes|
+      new_languages[name] ||= attributes
+      new_languages[name][:style] ||= []
+      new_languages[name][:style] << type
+    end
+  end
+  new_languages
 end
