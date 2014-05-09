4608f5c8864845becb2825baa730f88f25559a9e
Blake Johnson <blake41@gmail.com>
2014-05-09 14:06:13 -0400

    more descriptive soltuion

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,37 @@
+# def reformat_languages(languages)
+#   new_languages = {}
+#   languages.each do |type, hash|
+#     hash.each do |name, attributes|
+#       new_languages[name] ||= attributes
+#       new_languages[name][:style] ||= []
+#       new_languages[name][:style] << type
+#     end
+#   end
+#   new_languages
+# end
+
 def reformat_languages(languages)
-  new_languages = {}
-  languages.each do |type, hash|
-    hash.each do |name, attributes|
-      new_languages[name] ||= attributes
-      new_languages[name][:style] ||= []
-      new_languages[name][:style] << type
+  language_attributes = {}
+
+  languages.each do |oo_or_functional, language_hash|
+    #:oo, {:ruby => {...}}
+    language_hash.each do |language, attribute_hash|
+      #:ruby, {:type => "interpreted"}
+      attribute_hash.each do |attribute, str_value|
+        #:type, "interpreted"
+        if language_attributes[language].nil?
+          #if language :ruby doesn't exist in new hash
+          language_attributes[language] = {}  #initiate a new hash for the value of :ruby
+        end
+        # this would cause us problems if we had more than one attribute we're iterating over
+        language_attributes[language][:style] ||= []  # create a :style key within that hash and set it equal to an empty array
+        language_attributes[language][:style] << oo_or_functional  #added oo key from first level of languages hash to this key                                
+        if language_attributes[language][attribute].nil?
+          #if :ruby language key doesn't have a :type key nested within it
+          language_attributes[language][attribute] = str_value  #set this :type key to equal the str "interpreted"
+        end
+      end
     end
   end
-  new_languages
+  language_attributes
 end
